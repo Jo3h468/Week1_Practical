43c2941b88266adf822043ff2df5d4df7d650940
csf23moj <csf23moj@MF116PC33>
2024-09-24 13:57:08 +0100

    Create a C# console application that prints a message to the user asking for their name and age. After taking user input, display their name and how old they will be in 5 years.

---
+++ Week 1 Practical/Task1.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter you name");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter you age");
+int age =  Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Hello " + name + " you will be " + (age+= 5) + " in 5 years");
